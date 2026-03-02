9d2caabcd4f7fca338271408da0aed564407aee5
Ayan Kassen <ayan.kassen@bell-sw.com>
2026-03-02 21:10:47 +0500

    Fix/memories imgs

--- src/app/(main)/memories/page.tsx
+++ src/app/(main)/memories/page.tsx
@@ -4,6 +4,15 @@ import CoverRow from "@/components/CapturedMemories"
 
 import bgMusic from '@/images/bgMusic.png'
 
+import csImg from '@/images/memories/1.png'
+import walkImg from '@/images/memories/2.png'
+import eveningsImg from '@/images/memories/3.png'
+import clutchImg from '@/images/memories/4.png'
+import campingImg from '@/images/memories/5.png'
+import healthcareImg from '@/images/memories/6.png'
+import codingImg from '@/images/memories/7.png'
+import makeupImg from '@/images/memories/8.png'
+
 export const metadata = {
     title: "For my love | Memories page",
     description: "Memories page",
@@ -29,42 +38,42 @@ export default function Index() {
                         {
                             title: 'Love is playing CS:GO together.',
                             type: 'image',
-                            preview: { src: '/images/1.png', alt: '' }
+                            preview: { src: csImg.src, alt: '' }
                         },
                         {
                             title: 'Love is a walk through the village corners.',
                             type: 'image',
-                            preview: { src: '/images/2.png', alt: '' }
+                            preview: { src: walkImg.src, alt: '' }
                         },
                         {
                             title: 'Love is cozzy evenings together.',
                             type: 'image',
-                            preview: { src: '/images/3.png', alt: '' }
+                            preview: { src: eveningsImg.src, alt: '' }
                         },
                         {
                             title: 'Love is winning a 1 on 5 clutch match while she watches.',
                             type: 'image',
-                            preview: { src: '/images/4.png', alt: '' }
+                            preview: { src: clutchImg.src, alt: '' }
                         },
                         {
                             title: 'Love is camping together in the mouintains.',
                             type: 'image',
-                            preview: { src: '/images/5.png', alt: '' }
+                            preview: { src: campingImg.src, alt: '' }
                         },
                         {
                             title: 'Love is when IT and healthcare make the perfect combination.',
                             type: 'image',
-                            preview: { src: '/images/6.png', alt: '' }
+                            preview: { src: healthcareImg.src, alt: '' }
                         },
                         {
                             title: "Love is coding a website for her on Valentine's.",
                             type: 'image',
-                            preview: { src: '/images/7.png', alt: '' }
+                            preview: { src: codingImg.src, alt: '' }
                         },
                         {
                             title: "Love is watching her do her makeup with admiraion.",
                             type: 'image',
-                            preview: { src: '/images/8.png', alt: '' }
+                            preview: { src: makeupImg.src, alt: '' }
                         }
                     ]}
                 />
