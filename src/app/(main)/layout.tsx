e4ad08d6617b4157354e2808f7413ae9e0288492
Ayan Kassen <ayan.kassen@bell-sw.com>
2026-07-22 05:40:32 +0500

    Fixed font

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -5,7 +5,7 @@ import '@/styles/styles.scss'
 import { Quicksand } from 'next/font/google'
 
 const quicksand = Quicksand({
-  subsets: ['latin', 'cyrillic'],
+  subsets: ['latin'],
   weight: ['400', '500', '600', '700']
 })
 
